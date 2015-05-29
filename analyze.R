9bdfbe9b2f5450a0d959234c717ebeda7444aefe
unknown <iasr195@INFO-DS0063414.upcorp.ad.uprr.com>
2015-05-29 14:14:20 -0500

    Found one line that does it better than what I was doing

--- analyze.R
+++ analyze.R
@@ -6,13 +6,6 @@ analyze = function(){
   colnames(data) = lapply(data[1,], as.character)
   data = data[-1,]
   
-  r = matrix(nrow = length(levels(data[,sortBy])))
-  
-  i = 0
-  for(x in levels(data[, sortBy])){
-    d = subset(data, x == sortBy) 
-    i = i + 1
-  }
-    
-  
+  as.data.frame(tapply(as.numeric(data[,"TRAN AMT"]), data[,sortBy], mean))
+
 }
