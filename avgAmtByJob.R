da4983586a4820bf761d9e5a875a90e13c286a5b
unknown <iasr195@INFO-DS0063414.upcorp.ad.uprr.com>
2015-05-29 17:08:53 -0500

    I think the averages are all calculated. Returns it as a 4 part list with the 3rd as a matrix

--- avgAmtByJob.R
+++ avgAmtByJob.R
@@ -1,4 +1,4 @@
-avgAmtByJob = function(job, cardData, purchaseData){
+avgAmtByJob = function(job, cardData, purchaseData, wildcards = c("gift", "camera", "ipad", "ipod", "tv")){
   
   people = read.csv(cardData, header = T)
   
@@ -9,14 +9,18 @@ avgAmtByJob = function(job, cardData, purchaseData){
   ids = subset(people, people$DEPARTMENT == job)$EMPL_ID #TODO: check department is the field I want
   #TODO: ensure I care about case sensitivity
   
-  amounts = subset(purchases, purchases$"EMPL ID" %in% ids, select = c('EMPL ID', 'TRAN AMT'))
+  amounts = subset(purchases, purchases$"EMPL ID" %in% ids, select = c('EMPL ID', 'TRAN AMT', 'COST CODE', 'EXPENSE DESC'))
   amounts = droplevels(amounts)
 
   m = mean(as.numeric(amounts$'TRAN AMT'), na.rm = T)
   
   f = mean(tapply(amounts$'EMPL ID', amounts$'EMPL ID', length), na.rm = T)
- 
-  c(m, f)
   
+  r = tapply(amounts$'EMPL ID', list(amounts$'EMPL ID', amounts$'COST CODE'), length)
+  r[is.na(r)] = 0
+  r = colMeans(r) #note that if it's not listed in R that means no person in the job has purchased from this relater type
   
+  w = mean(as.numeric(sapply(amounts$'EXPENSE DESC', function(x) grepl(paste(wildcards, collapse = "|"), x))))
+  
+  list(m, f, r, w)
 }
