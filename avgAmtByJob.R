2bf4c6b8cb4c16e754525e78b67da47bb4b0a19f
unknown <iasr195@INFO-DS0063414.upcorp.ad.uprr.com>
2015-06-01 09:51:45 -0500

    Added method avgAmtByID to determine an individuals average amounts.
    Also fixed the mean for the average payment because factors can't go straight to numbers apperently

--- avgAmtByJob.R
+++ avgAmtByJob.R
@@ -12,7 +12,7 @@ avgAmtByJob = function(job, cardData, purchaseData, wildcards = c("gift", "camer
   amounts = subset(purchases, purchases$"EMPL ID" %in% ids, select = c('EMPL ID', 'TRAN AMT', 'COST CODE', 'EXPENSE DESC'))
   amounts = droplevels(amounts)
 
-  m = mean(as.numeric(amounts$'TRAN AMT'), na.rm = T)
+  m = mean(as.numeric(as.character(amounts$'TRAN AMT')), na.rm = T)
   
   f = mean(tapply(amounts$'EMPL ID', amounts$'EMPL ID', length), na.rm = T)
   
@@ -23,4 +23,28 @@ avgAmtByJob = function(job, cardData, purchaseData, wildcards = c("gift", "camer
   w = mean(as.numeric(sapply(amounts$'EXPENSE DESC', function(x) grepl(paste(wildcards, collapse = "|"), x))))
   
   list(m, f, r, w)
+}
+
+avgAmtByID = function(id, purchaseData, wildcards = c("gift", "camera", "ipad", "ipod", "tv")){
+  
+  purchases = read.csv(purchaseData)
+  colnames(purchases) = lapply(purchases[1,], as.character)
+  purchases = purchases[c(-1,-2),]
+  
+  
+  amounts = subset(purchases, purchases$"EMPL ID" == id, select = c('EMPL ID', 'TRAN AMT', 'COST CODE', 'EXPENSE DESC'))
+  amounts = droplevels(amounts)
+  
+  m = mean(as.numeric(as.character(amounts$'TRAN AMT')), na.rm = T)
+  
+  f = nrow(amounts)
+  
+  r = tapply(amounts$'EMPL ID', list(amounts$'EMPL ID', amounts$'COST CODE'), length)
+  r[is.na(r)] = 0
+  r = colMeans(r) #note that if it's not listed in R that means no person in the job has purchased from this relater type
+  
+  w = mean(as.numeric(sapply(amounts$'EXPENSE DESC', function(x) grepl(paste(wildcards, collapse = "|"), x))))
+  
+  list(m, f, r, w)
+  
 }
