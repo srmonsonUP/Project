700a7aafa0c0fd3c7af1e1bbbb7f4a76f1c913be
unknown <iasr195@INFO-DS0063414.upcorp.ad.uprr.com>
2015-06-02 14:43:45 -0500

    Creates a plot for average amount spent for each item type, vendor, and location combination.
    Not sure how to graphically show this yet

---
+++ amtPerTypeVendLoc.R
@@ -0,0 +1,31 @@
+amtPerTypeVendLoc = function(){
+  
+  if(!checkLoad())
+    load("Card Data.csv", "Purchasing Card Data.csv")
+  
+  clearPlot()
+  
+  purchases2 = purchases[order(purchases[,type], purchases[,vendor], purchases[,location]),]
+  
+  curType = -1
+  curVend = -1
+  curLoc = -1
+  avgPerTypeVendLoc = c()
+  for(x in 1:nrow(purchases2)){
+    if(curType != purchases2[x, type] || curVend != purchases2[x, vendor] || curLoc != purchases2[x, location]){
+      curType = purchases2[x, type]
+      curVend = purchases2[x, vendor]
+      curLoc = purchases2[x, location]   
+      
+      avg = subset(purchases2, purchases2[,type] == curType & purchases2[,vendor] == curVend & purchases[,location] == curLoc, select = 'TRAN AMT')
+      avg = mean(as.numeric(as.character(avg$'TRAN AMT')), na.rm = T)
+      avgPerTypeVendLoc = c(avgPerTypeVendLoc, paste(curType, curVend, curLoc, sep = "."), avg)
+    }
+  }
+  avgPerTypeVendLoc = matrix(avgPerTypeVendLoc, ncol = 2, byrow = T)
+  plot(as.numeric(avgPerTypeVendLoc[,1]), as.numeric(avgPerTypeVendLoc[,2]), xlab = "Cost Code, SIC Code, State", ylab = "Mean Amt", main = "Mean Amt / (Cost Code, SIC Code State)")
+}
+
+tonum = function(type, vend, loc){
+  
+}
