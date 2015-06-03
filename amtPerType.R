6c7990d44e66c68962685242e1f1cc3c3957f85a
unknown <iasr195@INFO-DS0063414.upcorp.ad.uprr.com>
2015-06-03 10:52:57 -0500

    Cleaned up the code to be more efficient and condenced.
    Also ploting the x axis as factors

--- amtPerType.R
+++ amtPerType.R
@@ -4,21 +4,15 @@ amtPerType = function(){
     load("Card Data.csv", "Purchasing Card Data.csv")
   
   clearPlot()
-  
-  type = "COST CODE"
+  clean()
   
   purchases2 = purchases[order(purchases[,type]),]
   
   avgPerType = c()
   cur = -1
-  for(x in purchases2[,type]){
-    if(cur != x){
-      cur = x
-      avg = subset(purchases, purchases2[,type] == x, select = 'TRAN AMT')
-      avg = mean(as.numeric(as.character(avg$'TRAN AMT')), na.rm = T)
-      avgPerType = c(avgPerType, x, avg)
-    }
-  }
-  avgPerType = matrix(avgPerType, ncol = 2, byrow = T)
-  plot(avgPerType[,1], avgPerType[,2], xlab = "Cost Code", ylab = "Mean Amt", main = "Mean Amt / Cost Code")
+  
+  avg = tapply(purchases2[,'TRAN AMT'], purchases2[,type], mean, na.rm = T)
+
+  avgPerType = matrix(c(names(avg), as.vector(avg)), ncol = 2)
+  plot(as.factor(avgPerType[,1]), avgPerType[,2], xlab = "Cost Code", ylab = "Mean Amt", main = "Mean Amt / Cost Code")
 }
