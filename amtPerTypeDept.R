9cc7db52a920e575fe422275f56cb4adcb05d249
unknown <iasr195@INFO-DS0063414.upcorp.ad.uprr.com>
2015-06-03 10:53:30 -0500

    A working but super inefficient method

--- amtPerTypeDept.R
+++ amtPerTypeDept.R
@@ -5,22 +5,30 @@ amtPerItemDept = function(){
   
   clearPlot()
   
-  purchases2 = purchases[order(purchases[,type]),]
+#   purchases2 = purchases[order(purchases[,type]),]
+  people2 = people[order(people[,department]),]
   
   curType = -1
   curDept = -1
-  for(x in purchases2[,type]){
-    if(curType != x){
-      curType = x
-      t = purchases2[purchases2[,type] == curType, ]
-      for(y in t$'EMPL ID'){
-        
+  avgPerTypeDept = c()
+  for(x in people2[, department]){
+    if(curDept != x){
+      curDept = x
+      ids = people2[people[,department] == curDept, 'EMPL_ID']
+      purchases2 = purchases[purchases$'EMPL ID' %in% ids, ]
+     
+      for(y in purchases2[,type]){
+        if(curType != y){
+          curType = y
+          avg = subset(purchases, purchases2[,type] == y, select = 'TRAN AMT')
+          avg = mean(as.numeric(as.character(avg$'TRAN AMT')), na.rm = T)
+          avgPerTypeDept = c(avgPerTypeDept, as.character(curType), curDept, avg)
+        }
       }
+      curType = -1
     }
   }
-}
-
-deptByID = function(id){
-  dep = as.character(people[people$'EMPL_ID' == id, department])[1]
-  people[people[,department] == dep, ] #ids for said dept
+  avgPerTypeDept = matrix(avgPerTypeDept, ncol = 3, byrow = T)
+  plot(as.numeric(avgPerTypeDept[,1]), avgPerTypeDept[,3], col = as.numeric(as.factor(avgPerTypeDept[,2])), xlab = "Cost Code", ylab = "Mean Amt")
+  legend(x = "right", legend = unique(avgPerTypeDept[,2]), col = as.numeric(unique(avgPerTypeDept[,2])), cex = .5)
 }
