eaf5a70f5417aa450e1cf6c2c847d24dda9be400
unknown <iasr195@INFO-DS0063414.upcorp.ad.uprr.com>
2015-06-04 13:19:51 -0500

    Fixed whatever I broke. The createCategories() should be working. I also drop the really large value outlier.

--- amtPerType.R
+++ amtPerType.R
@@ -17,30 +17,32 @@ amtPerType = function(){
   values = as.numeric(as.character(lapply(1:length(categories), function(x) categories[[x]][[4]])))
   levels(names) = names
   plot(names, values, type = 'n', lty = NULL)
-  text(names, values+1000, round(values, 2), cex=0.8)
-  avgPerType
+  text(names, values+100, round(values), cex=0.8)
 }
+
 
 createCategories = function(data){
   
   thresh = 0.05
   categories = c()
   
-  if(!is.numeric(data[,2])){
-    data[,2] = as.numeric(as.character(data[,2]))
-  }
+#   if(!is.numeric(data[,2])){                              I tried to do this to reduce
+#     data[,2] = as.numeric(as.character(data[,2]))         the conversions below
+#   }                                                       but it didn't seem to work
   
   count = 0
   while(nrow(data) > 0){
-    val = data[1,2]
+    val = as.numeric(as.character(data[1,2]))
     min = val - val * thresh
     max = val + val * thresh
     
-    cat = data[data[,2] >= min & data[,2] <= max, ]
+    cat = data[as.numeric(as.character(data[,2])) >= min & as.numeric(as.character(data[,2])) <= max, ]
     cat = c(cat, c(paste("Value ", LETTERS[floor(count/26)], LETTERS[count%%26 + 1], sep = ""), val))
     count = count + 1
-    data = subset(data, !(data[,2] >= min & data[,2] <= max))
-    categories = c(categories, list(cat))
+    data = subset(data, !(as.numeric(as.character(data[,2])) >= min & as.numeric(as.character(data[,2])) <= max))
+    
+    if(cat[[4]] < 50000) #Temporarily remove outlier
+      categories = c(categories, list(cat))
   }
   categories
   
