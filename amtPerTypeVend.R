af0000694767145c72cae05ba23bdca6142df5f0
unknown <iasr195@INFO-DS0063414.upcorp.ad.uprr.com>
2015-06-04 13:20:35 -0500

    Cleaned up code
    Trying to make categories based on the categories of the two codes

--- amtPerTypeVend.R
+++ amtPerTypeVend.R
@@ -6,22 +6,35 @@ amtPerTypeVend = function(){
     load("Card Data.csv", "Purchasing Card Data.csv")
   
   clearPlot()
+  clean()
+ 
+  avg = tapply(purchases$'TRAN AMT', list(purchases[,type], purchases[,vendor]), mean, na.rm=T)
   
-  purchases2 = purchases[order(purchases[,type], purchases[,vendor]),]
+  x = which(avg > 0, arr.ind = T)
   
   avgPerTypeVend = c()
-  curType = -1
-  curVend = -1
-  for(x in 1:nrow(purchases2)){
-    if(curType != purchases2[x, type] || curVend != purchases2[x, vendor]){
-      curType = purchases2[x, type] #may need to convert to numeric
-      curVend = purchases2[x, vendor]
-      avg = subset(purchases2, purchases2[,type] == curType & purchases2[,vendor] == curVend, select = 'TRAN AMT')
-      avg = mean(as.numeric(as.character(avg$'TRAN AMT')), na.rm = T)
-      avgPerTypeVend = c(avgPerTypeVend, c(as.character(curType), as.character(curVend), avg))
-    }
+  for(i in 1:nrow(x)){
+    avgPerTypeVend = c(avgPerTypeVend, c(rownames(avg)[x[i,1]], colnames(avg)[x[i,2]], avg[x[i,1], x[i,2]]))
   }
   avgPerTypeVend = matrix(avgPerTypeVend, ncol = 3, byrow = T)
-  plot(as.numeric(avgPerTypeVend[,1]), as.numeric(avgPerTypeVend[,3]), col = avgPerTypeVend[,2], xlab = "Cost Code", ylab = "Mean Amt") #TODO: check what the color thing is being set to
-  legend(x = "bottomright", legend = avgPerTypeVend[,2], col = avgPerTypeVend[,2])
+  avgPerTypeVend[,1] = as.factor(avgPerTypeVend[,1])
+  avgPerTypeVend[,2] = as.factor(avgPerTypeVend[,2])
+#   plot3d(avgPerTypeVend, xlab = "SIC Code", ylab = "Cost Code", zlab = "Mean Amt")
+  createCategories2(avgPerTypeVend)
 }
+
+createCategories2 = function(data){
+  
+  vendCat = createCategories(data[,c(1,3)])
+  typeCat = createCategories(matrix(data[,2], data[,3], ncol = 2))
+  
+  while(nrow(data) > 0){
+    vendCatName = getCatName(data[1,1], vendCat)
+    typeCatName = getCatName(data[1,2], typeCat)
+    
+    cat = data[getCatName(data[,1], vendCat) == vendCatName & getCatName(data[,2], typeCat) == typeCatName]
+    data = subset(data, !(getCatName(data[,1], vendCat) == vendCatName & getCatName(data[,2], typeCat) == typeCatName))
+    
+  }
+}
+
