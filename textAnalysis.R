721c99f4938bc19c9264bd754361dec7bb2bd1d6
unknown <iasr195@INFO-DS0063414.upcorp.ad.uprr.com>
2015-06-05 16:47:24 -0500

    Gives item description and its frequency of use

--- textAnalysis.R
+++ textAnalysis.R
@@ -6,8 +6,13 @@ textAnalysis = function(){
   
    unique(level$ITEM.DESCRIPTION)
    
-   count = tapply(purchases2$'TRAN AMT', list(purchases2[,department], purchases2[,vendor]), function(x) length(x))
+   count = tapply(level$EMPL.ID, level$ITEM.DESCRIPTION, function(x) length(x))
    
-   
-   
+#    matrix(names(count), as.vector(count), ncol = 2)
+#    
+#    for(i in 1:nrow()){
+#      deptVendComb = c(deptVendComb, c(rownames(count)[x[i,1]], colnames(count)[x[i,2]], count[x[i,1], x[i,2]]))
+#    }
+
+  aggregate(level$ITEM.DESCRIPTION, by = list(level$ITEM.DESCRIPTION), FUN = length)
 }
