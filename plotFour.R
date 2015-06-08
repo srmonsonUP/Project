793bac57ee67d1a7cbf335d57845153a4a022388
unknown <iasr195@INFO-DS0063414.upcorp.ad.uprr.com>
2015-06-08 11:07:02 -0500

    Takes category data and plots it. Also loops through line by line and plots an individual purchase on the graphs.
    Last one not currenly working

--- plotFour.R
+++ plotFour.R
@@ -3,22 +3,63 @@ library(ggplot2)
 plotFour = function(){
   
   amtPerDept = amtPerDept()
+  
   amtPerType = amtPerType()
-  amtPerVend = amtPerVend()
-  amtPerTypeVend = amtPerTypeVend()
+  typeNames = as.factor(as.character(lapply(1:length(amtPerType), function(x) amtPerType[[x]][[3]])))
+  typeValues = as.numeric(as.character(lapply(1:length(amtPerType), function(x) amtPerType[[x]][[4]])))
+  levels(typeNames) = typeNames
+  
+  amtPerVend = amtPerVend()  
+  vendNames = as.factor(as.character(lapply(1:length(amtPerVend), function(x) amtPerVend[[x]][[3]])))
+  vendValues = as.numeric(as.character(lapply(1:length(amtPerVend), function(x) amtPerVend[[x]][[4]])))
+  levels(vendNames) = vendNames
+  
+  amtPerTypeVend = amtPerTypeVend()  
+  typeVendNames = as.factor(as.character(lapply(1:length(amtPerTypeVend), function(x) amtPerTypeVend[[x]][[3]])))
+  typeVendValues = as.numeric(as.character(lapply(1:length(amtPerTypeVend), function(x) amtPerTypeVend[[x]][[4]])))
+  levels(typeVendNames) = typeVendNames
 
-  clearPlot()
-  par(mfrow = c(2,2))
   
-  plot(as.factor(amtPerDept[,1]), amtPerDept[,2], main = "Amount per Department")
-  text(amtPerDept[,1],amtPerDept[,2]+50, round(amtPerDept[,2]), cex = 0.8)
+  purchases2 = merge(purchases, people, by.x = 'EMPL ID', by.y = 'EMPL_ID')
+  cont = 'y'
+  i = 1
+  while(cont == 'y'){
+    clearPlot()
+    par(mfrow = c(2,2))
+    
+    plot(as.factor(amtPerDept[,1]), amtPerDept[,2], main = "Amount per Department")
+    text(amtPerDept[,1],amtPerDept[,2]+50, round(amtPerDept[,2]), cex = 0.8)
+    points(x = purchases2[i, department], y = purchases2[i, 'TRAN AMT'], col = 'red')
+    
+    
+    plot(typeNames, typeValues, main = "Amount per Cost Code")
+    text(typeNames, typeValues+500, round(typeValues), cex=0.8)
+    section = findSection(amtPerType, purchases2[i, type])
+    points(x = match(section, levels(typeNames)), y = purchases2[i, 'TRAN AMT'], col = 'red')
+    
+    
+    plot(vendNames, vendValues, main = "Amount per SIC Code")
+    text(vendNames, vendValues+500, round(vendValues), cex=0.8)
+    section = findSection(amtPerVend, purchases2[i, vendor])
+    points(x = match(section, levels(vendNames)), y = purchases2[i, 'TRAN AMT'], col = 'red')
+    
+    
+    plot(typeVendNames, typeVendValues, main = "Amount per Cost Code and SIC Code")
+    text(typeVendNames, typeVendValues+300, round(typeVendValues), cex=0.8)
+    section = findSection(amtPerTypeVend, as.numeric(paste(purchases2[i, type], purchases2[i, vendor], sep = '.')))
+    points(x = section, y = purchases2[i, 'TRAN AMT'], col = 'red')
+    
+    
+    cont = readline("Plot next? (y/n) \n")
+    i = i + 1
+  }
+}
+
+findSection = function(data, value){
   
-  plot(as.factor(amtPerType[,1]), amtPerType[,2], main = "Amount per Cost Code")
-  text(amtPerType[,1],amtPerType[,2]+500, round(amtPerType[,2]), cex = 0.8)
+  for(i in 1:length(data)){
+    if(value %in% data[[i]][[1]])
+      return(data[[i]][[3]])
+  }
   
-  plot(as.factor(amtPerVend[,1]), amtPerVend[,2], main = "Amount per SIC Code")
-  text(amtPerVend[,1],amtPerVend[,2]+750, round(amtPerVend[,2]), cex = 0.8)
-  
-  plot(as.factor(amtPerTypeVend[,1]), amtPerTypeVend[,2], main = "Amount per Cost Code and SIC Code")
-  text(amtPerTypeVend[,1],amtPerTypeVend[,2]+1000, round(amtPerTypeVend[,2]), cex = 0.8)
 }
