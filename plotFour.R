b17b3dc1adb5dd28201b148bf3d409e264af098b
unknown <iasr195@INFO-DS0063414.upcorp.ad.uprr.com>
2015-06-08 16:44:00 -0500

    Changed to use standard deviation data as the text output

--- plotFour.R
+++ plotFour.R
@@ -4,62 +4,86 @@ plotFour = function(){
   
   amtPerDept = amtPerDept()
   
+  
   amtPerType = amtPerType()
   typeNames = as.factor(as.character(lapply(1:length(amtPerType), function(x) amtPerType[[x]][[3]])))
   typeValues = as.numeric(as.character(lapply(1:length(amtPerType), function(x) amtPerType[[x]][[4]])))
-  levels(typeNames) = typeNames
+  typeDev = as.numeric(as.character(lapply(1:length(amtPerType), function(x) amtPerType[[x]][[5]])))
+  typeData = cbind.data.frame(typeNames, typeValues, typeDev)
+  typeData[,1] = factor(typeData[,1], typeData[,1])
   
+    
   amtPerVend = amtPerVend()  
   vendNames = as.factor(as.character(lapply(1:length(amtPerVend), function(x) amtPerVend[[x]][[3]])))
   vendValues = as.numeric(as.character(lapply(1:length(amtPerVend), function(x) amtPerVend[[x]][[4]])))
-  levels(vendNames) = vendNames
+  vendDev = as.numeric(as.character(lapply(1:length(amtPerVend), function(x) amtPerVend[[x]][[4]])))
+  vendData = cbind.data.frame(vendNames, vendValues, vendDev)
+  vendData[,1] = factor(vendData[,1], vendData[,1]) 
   
   amtPerTypeVend = amtPerTypeVend()  
   typeVendNames = as.factor(as.character(lapply(1:length(amtPerTypeVend), function(x) amtPerTypeVend[[x]][[3]])))
   typeVendValues = as.numeric(as.character(lapply(1:length(amtPerTypeVend), function(x) amtPerTypeVend[[x]][[4]])))
-  levels(typeVendNames) = typeVendNames
-
+  typeVendDev = as.numeric(as.character(lapply(1:length(amtPerTypeVend), function(x) amtPerTypeVend[[x]][[5]])))
+  typeVendData = cbind.data.frame(typeVendNames, typeVendValues, typeVendDev)
+  typeVendData[,1] = factor(typeVendData[,1], typeVendData[,1])
   
   purchases2 = merge(purchases, people, by.x = 'EMPL ID', by.y = 'EMPL_ID')
   cont = ''
-  i = 1
+  i = 20
   while(cont == ''){
     clearPlot()
-    par(mfrow = c(2,2))
+    par(mfrow = c(2,2), las = 2)
     
     plot(as.factor(amtPerDept[,1]), amtPerDept[,2], main = "Amount per Department")
     text(amtPerDept[,1],amtPerDept[,2]+50, round(amtPerDept[,2]), cex = 0.8)
-    points(x = purchases2[i, department], y = purchases2[i, 'TRAN AMT'], col = 'red')
+    points(x = purchases2[i, department], y = purchases2[i, 'TRAN AMT'], col = 'red', cex = 1.5)
+    sdev = (purchases2[i, 'TRAN AMT'] - amtPerDept[amtPerDept[,1] == purchases2[i, department],2]) / amtPerDept[amtPerDept[,1] ==purchases2[i, department],3]
+    usr = par("usr")
+    text(usr[1], usr[4], paste("SD =", round(sdev, 2)), col = 'red', cex = 1.5, adj = c(0,1))
     
     
-    plot(typeNames, typeValues, main = "Amount per Cost Code")
-    text(typeNames, typeValues+500, round(typeValues), cex=0.8)
+    plot(typeData[,1], typeData[,2], main = "Amount per Cost Code")
+    text(typeData[,1], typeData[,2]+500, round(typeData[,2]), cex=0.8)
     section = findSection(amtPerType, purchases2[i, type])
-    points(x = match(section, levels(typeNames)), y = purchases2[i, 'TRAN AMT'], col = 'red')
+    points(x = typeData[typeData[,1] == section, 1], y = purchases2[i, 'TRAN AMT'], col = 'red', cex = 1.5)
+    sdev = (purchases2[i, 'TRAN AMT'] - typeData[typeData[,1] == section, 2]) / typeData[typeData[,1] == section, 3]
+    usr = par("usr")
+    text(usr[1], usr[4], paste("SD =", round(sdev, 2)), col = 'red', cex = 1.5, adj = c(0,1))
     
     
-    plot(vendNames, vendValues, main = "Amount per SIC Code")
-    text(vendNames, vendValues+500, round(vendValues), cex=0.8)
+    plot(vendData[,1], vendData[,2], main = "Amount per SIC Code")
+    text(vendData[,1], vendData[,2]+500, round(vendData[,2]), cex=0.8)
     section = findSection(amtPerVend, purchases2[i, vendor])
-    points(x = match(section, levels(vendNames)), y = purchases2[i, 'TRAN AMT'], col = 'red')
+    points(x = vendData[vendData[,1] == section, 1], y = purchases2[i, 'TRAN AMT'], col = 'red', cex = 1.5)
+    sdev = (purchases2[i, 'TRAN AMT'] - vendData[vendData[,1] == section, 2]) / vendData[vendData[,1] == section, 3]
+    usr = par("usr")
+    text(usr[1], usr[4], paste("SD =", round(sdev, 2)), col = 'red', cex = 1.5, adj = c(0,1))
     
     
-    plot(typeVendNames, typeVendValues, main = "Amount per Cost Code and SIC Code")
-    text(typeVendNames, typeVendValues+300, round(typeVendValues), cex=0.8)
+    plot(typeVendData[,1], typeVendData[,2], main = "Amount per Cost Code and SIC Code")
+    text(typeVendData[,1], typeVendData[,2]+300, round(typeVendData[,2]), cex=0.8)
     section = findSection(amtPerTypeVend, as.numeric(paste(purchases2[i, type], purchases2[i, vendor], sep = '.')))
-    points(x = match(section, levels(typeVendNames)), y = purchases2[i, 'TRAN AMT'], col = 'red')
+    points(x = typeVendData[typeVendData[,1] == section,1], y = purchases2[i, 'TRAN AMT'], col = 'red', cex = 1.5)
+    sdev = (purchases2[i, 'TRAN AMT'] - typeVendData[typeVendData[,1] == section, 2]) / typeVendData[typeVendData[,1] == section, 3]
+    usr = par("usr")
+    text(usr[1], usr[4], paste("SD =", round(sdev, 2)), col = 'red', cex = 1.5, adj = c(0,1))
     
     
-    cont = readline("Plot next? Any character to quit")
+   
     i = i + 1
+    cont = readline("Plot next? Any character to quit")
   }
 }
 
 findSection = function(data, value){
-  
   for(i in 1:length(data)){
-    if(value %in% data[[i]][[1]])
+    if(value %in% as.numeric(as.character(data[[i]][[1]])))
       return(data[[i]][[3]])
   }
-  
+}
+
+findMean = function(data, value){
+  for(i in 1:length(data))
+    if(value == data[[i]][[3]])
+      return(as.numeric(data[[i]][[4]]))
 }
