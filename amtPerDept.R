75a943c98b62196d45404abc6b14869cb767c120
unknown <iasr195@INFO-DS0063414.upcorp.ad.uprr.com>
2015-06-08 16:42:58 -0500

    Added column for standard deviation. Works

--- amtPerDept.R
+++ amtPerDept.R
@@ -10,12 +10,11 @@ amtPerDept = function(){
   purchases2 = merge(purchases, people, by.x = 'EMPL ID', by.y = 'EMPL_ID')
   
   avg = tapply(purchases2$'TRAN AMT', purchases2[, department], mean, na.rm = T)
+  sd = tapply(purchases2$'TRAN AMT', purchases2[, department], sd, na.rm = T)
   
-  avgPerDept = as.data.frame(matrix(c(names(avg), as.vector(avg)), ncol = 2))
+  avgPerDept = as.data.frame(matrix(c(names(avg), as.vector(avg), as.vector(sd)), ncol = 3))
   avgPerDept[,2] = as.numeric(as.character(avgPerDept[,2]))
-  
-  plot(as.factor(avgPerDept[,1]), avgPerDept[,2])
-  text(avgPerDept[,1],avgPerDept[,2]+50, round(avgPerDept[,2]), cex = 0.8)
+  avgPerDept[,3] = as.numeric(as.character(avgPerDept[,3]))
   
   return(avgPerDept)
 }
