f5fcc00a12ba8b24db571a2f3557b6466c78c475
unknown <iasr195@INFO-DS0063414.upcorp.ad.uprr.com>
2015-06-10 13:54:16 -0500

    Cleaned up the code. Set up method to go through purhcases line by line and show where that purchase sits on the graphs

--- boxplotFour.R
+++ boxplotFour.R
@@ -1,25 +1,57 @@
 boxplotFour = function(){
   
   dep = boxPlotData(department)
+  gdep = lapply(dep[,3], function(x) x)
+  gdep = do.call(mapply, c(cbind, gdep))
+  gdep$names = dep[,1]
+
   typ = boxPlotData(type)
+  gtyp = lapply(typ[,3], function(x) x)
+  gtyp = do.call(mapply, c(cbind, gtyp))
+  gtyp$names = nameGen(length(typ[,1]))
+
   ven = boxPlotData(vendor)
-#   typVen = boxPlotData(c(type, vendor)) Doesn't work yet
+  gven = lapply(ven[,3], function(x) x)
+  gven = do.call(mapply, c(cbind, gven))
+  gven$names = nameGen(length(ven[,1]))
   
-  clearPlot()
-  par(mfrow = c(2,2), las = 2)
+  typVen = boxPlotData(c(type, vendor))
+  gtypven = lapply(typVen[,3], function(x) x)
+  gtypven = do.call(mapply, c(cbind, gtypven))
+  gtypven$names = nameGen(length(typVen[,1]))
 
-  tmp = lapply(dep[,3], function(x) x)
-  tmp = do.call(mapply, c(cbind, tmp))
-  bxp(tmp)
+  
+  purchases = merge(purchases, people, by.x = "EMPL ID", by.y = "EMPL_ID")
+  i = 1
+  c = ""
+  while(c == ""){
+    clearPlot()
+    par(mfrow = c(2,2), las = 2)
+    
+    amt = purchases[i, 'TRAN AMT']
+    
+    bxp(gdep, outline = F, main = "Transaction per Department", names = dep[,1])
+    points(1:length(dep[,2]), as.vector(unlist(dep[,2])))
 
-  tmp = lapply(typ[,3], function(x) x)
-  tmp = do.call(mapply, c(cbind, tmp))
-  bxp(tmp)
+    
+    bxp(gtyp, outline = F)
+    points(1:length(typ[,2]), as.vector(unlist(typ[,2])))
+    
+    bxp(gven, outline = F)
+    points(1:length(ven[,2]), as.vector(unlist(ven[,2])))
+    
+    bxp(gtypven, outline = F)
+    points(1:length(typVen[,2]), as.vector(unlist(typVen[,2])))
+    
+    i = i + 1
+    c = readline("Enter to cont")
+  }
+}
 
-  tmp = lapply(ven[,3], function(x) x)
-  tmp = do.call(mapply, c(cbind, tmp))
-  bxp(tmp)
-
-
-
+nameGen = function(len)
+{
+  n = c()
+  for(i in 1:len)
+    n = c(n, paste("Group ", LETTERS[floor(i/26)], LETTERS[i%%26], sep = ""))
+  n
 }
