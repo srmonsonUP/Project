58694f4d75c9960141580d132baa9486de7e81af
unknown <iasr195@INFO-DS0063414.upcorp.ad.uprr.com>
2015-06-10 15:38:19 -0500

    Function will now go line by line through purchases. Highlights purchases outside of the IQR

--- boxplotFour.R
+++ boxplotFour.R
@@ -3,7 +3,7 @@ boxplotFour = function(){
   dep = boxPlotData(department)
   gdep = lapply(dep[,3], function(x) x)
   gdep = do.call(mapply, c(cbind, gdep))
-  gdep$names = dep[,1]
+  gdep$names = lapply(dep[,1], function(x) strsplit(x, split=" ")[[1]][1][1])
 
   typ = boxPlotData(type)
   gtyp = lapply(typ[,3], function(x) x)
@@ -23,30 +23,91 @@ boxplotFour = function(){
   
   purchases = merge(purchases, people, by.x = "EMPL ID", by.y = "EMPL_ID")
   i = 1
-  c = ""
-  while(c == ""){
+  cont = ""
+  while(cont == ""){
     clearPlot()
     par(mfrow = c(2,2), las = 2)
+    c = 'green'
     
     amt = purchases[i, 'TRAN AMT']
     
     bxp(gdep, outline = F, main = "Transaction per Department", names = dep[,1])
     points(1:length(dep[,2]), as.vector(unlist(dep[,2])))
-
+    group = lapply(dep[,1], function(x) purchases[i, department] %in% x)
+    group = which(group == T, arr.ind = T)[1]
+    if(amt < dep[group, 3][[1]]$stats[2,1]){  #First Quartile
+      c = 'red'
+      usr = par("usr")
+      text(usr[1], usr[4], paste("Amount is", round(dep[group, 3][[1]]$stats[2,1] - amt, 2), "under first quartile"), col = c, cex = 1.5, adj = c(0,1))
+    } 
+    if(amt > dep[group, 3][[1]]$stats[4,1]){  #Third Quartile
+      c = 'red'
+      usr = par("usr")
+      text(usr[1], usr[4], paste("Amount is", round(amt - dep[group, 3][[1]]$stats[4,1], 2), "over third quartile"), col = c, cex = 1.5, adj = c(0,1))
+    }   
+    points(group, amt, col = c, cex = 1.5, pch = 15)
+    c = 'green'
+    
     
     bxp(gtyp, outline = F)
     points(1:length(typ[,2]), as.vector(unlist(typ[,2])))
+    group = lapply(typ[,1], function(x) purchases[i, type] %in% x)
+    group = which(group == T, arr.ind = T)[1]
+    if(amt < typ[group, 3][[1]]$stats[2,1]){  #First Quartile
+      c = 'red'
+      usr = par("usr")
+      text(usr[1], usr[4], paste("Amount is", round(typ[group, 3][[1]]$stats[2,1] - amt, 2), "under first quartile"), col = c, cex = 1.5, adj = c(0,1))
+    } 
+    if(amt > typ[group, 3][[1]]$stats[4,1]){  #Third Quartile
+      c = 'red'
+      usr = par("usr")
+      text(usr[1], usr[4], paste("Amount is", round(amt - typ[group, 3][[1]]$stats[4,1], 2), "over third quartile"), col = c, cex = 1.5, adj = c(0,1))
+    }   
+    points(group, amt, col = c, cex = 1.5, pch = 15)
+    c = 'green'
+    
     
     bxp(gven, outline = F)
     points(1:length(ven[,2]), as.vector(unlist(ven[,2])))
+    group = lapply(ven[,1], function(x) purchases[i, vendor] %in% x)
+    group = which(group == T, arr.ind = T)[1]
+    if(amt < ven[group, 3][[1]]$stats[2,1]){  #First Quartile
+      c = 'red'
+      usr = par("usr")
+      text(usr[1], usr[4], paste("Amount is", round(ven[group, 3][[1]]$stats[2,1] - amt, 2), "under first quartile"), col = c, cex = 1.5, adj = c(0,1))
+    } 
+    if(amt > ven[group, 3][[1]]$stats[4,1]){  #Third Quartile
+      c = 'red'
+      usr = par("usr")
+      text(usr[1], usr[4], paste("Amount is", round(amt - ven[group, 3][[1]]$stats[4,1], 2), "over third quartile"), col = c, cex = 1.5, adj = c(0,1))
+    }   
+    points(group, amt, col = c, cex = 1.5, pch = 15)
+    c = 'green'
+    
     
     bxp(gtypven, outline = F)
     points(1:length(typVen[,2]), as.vector(unlist(typVen[,2])))
+    group = lapply(typVen[,1], function(x) paste(purchases[i, type], purchases[i, vendor], sep = ".") %in% x)
+    group = which(group == T, arr.ind = T)[1]
+    if(amt < typVen[group, 3][[1]]$stats[2,1]){  #First Quartile
+      c = 'red'
+      usr = par("usr")
+      text(usr[1], usr[4], paste("Amount is", round(typVen[group, 3][[1]]$stats[2,1] - amt, 2), "under first quartile"), col = c, cex = 1.5, adj = c(0,1))
+    } 
+    if(amt > typVen[group, 3][[1]]$stats[4,1]){  #Third Quartile
+      c = 'red'
+      usr = par("usr")
+      text(usr[1], usr[4], paste("Amount is", round(amt - typVen[group, 3][[1]]$stats[4,1], 2), "over third quartile"), col = c, cex = 1.5, adj = c(0,1))
+    }   
+    points(group, amt, col = c, cex = 1.5, pch = 15)
+    c = 'green'
+    
     
     i = i + 1
-    c = readline("Enter to cont")
+    cont = readline("Enter to cont")
   }
 }
+
 
 nameGen = function(len)
 {
